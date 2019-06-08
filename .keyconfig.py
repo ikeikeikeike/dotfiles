1a81cf2c60c6ccdb87b99324ecdeed3fd3013c85
ikeikeikeike / Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2019-06-08 21:06:33 +0900

    added xkeysnail

---
+++ .keyconfig.py
@@ -0,0 +1,106 @@
+import re
+from xkeysnail.transform import *
+
+define_modmap({
+    Key.CAPSLOCK: Key.LEFT_CTRL,
+    # Key.LEFT_CTRL: Key.CAPSLOCK,
+})
+
+# noneed? at USKBD
+define_multipurpose_modmap({
+    Key.LEFT_META: [Key.MUHENKAN, Key.LEFT_META],
+})
+
+define_keymap(lambda wm_class: wm_class in ("Google-chrome", "albert"), {
+    K("M-w"): K("C-w"),
+    K("M-r"): K("C-r"),
+    K("M-t"): K("C-t"),
+    K("M-Shift-t"): K("C-Shift-t"),
+    K("M-o"): K("C-o"),
+    K("M-p"): K("C-p"),
+    K("M-a"): K("C-a"),
+    K("M-s"): K("C-s"),
+    K("M-f"): K("C-f"),
+    K("M-g"): K("C-g"),
+    K("M-z"): K("C-z"),
+    K("M-x"): K("C-x"),
+    K("M-c"): K("C-c"),
+    K("M-v"): K("C-v"),
+    K("M-n"): K("C-n"),
+    K("M-l"): K("C-l"),
+    K("M-LEFT"): K("C-LEFT"),
+    K("M-RIGHT"): K("C-RIGHT"),
+    K("M-LEFT_BRACE"): K("M-LEFT"),
+    K("M-RIGHT_BRACE"): K("M-RIGHT"),
+    K("M-Shift-LEFT"): K("C-Shift-LEFT"),
+    K("M-Shift-RIGHT"): K("C-Shift-RIGHT"),
+}, "Morden")
+
+define_keymap(lambda wm_class: wm_class in ("Boostnote"), {
+    K("M-w"): K("C-w"),
+    K("M-r"): K("C-r"),
+    K("M-t"): K("C-t"),
+    K("M-o"): K("C-o"),
+    K("M-p"): K("C-p"),
+    K("M-a"): K("C-a"),
+    K("M-s"): K("C-s"),
+    K("M-f"): K("C-f"),
+    K("M-g"): K("C-g"),
+    K("M-z"): K("C-z"),
+    K("M-x"): K("C-x"),
+    K("M-c"): K("C-c"),
+    K("M-v"): K("C-v"),
+    K("M-n"): K("C-n"),
+}, "Boostnote")
+
+define_keymap(lambda wm_class: wm_class in ("Gnome-terminal"), {
+    K("C-TAB"): K("C-PAGE_DOWN"),
+    K("C-Shift-TAB"): K("C-PAGE_UP"),
+    K("M-c"): K("C-Shift-c"),
+    K("M-v"): K("C-Shift-v"),
+    K("M-t"): K("C-Shift-t"),
+    K("M-w"): K("C-Shift-w"),
+    K("M-f"): K("C-Shift-f"),
+    K("M-g"): K("C-Shift-g"),
+    K("M-h"): K("C-Shift-h"),
+    K("M-j"): K("C-Shift-j"),
+}, "Gnome-terminal")
+
+
+define_keymap(lambda wm_class: wm_class not in ("Boostnote", "Emacs", "FocusProxy", "URxvt", "Gnome-terminal"), {
+    # Cursor
+    K("C-b"): with_mark(K("left")),
+    K("C-f"): with_mark(K("right")),
+    K("C-p"): with_mark(K("up")),
+    K("C-n"): with_mark(K("down")),
+    K("C-h"): with_mark(K("backspace")),
+    # Beginning/End of line
+    K("C-a"): with_mark(K("home")),
+    K("C-e"): with_mark(K("end")),
+    # Delete
+    K("C-d"): [K("delete"), set_mark(False)],
+    # Kill line
+    K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
+}, "Otherwise")
+
+
+define_keymap(None, {
+    K("C-LEFT_BRACE"): [K("C-LEFT_BRACE"), K("MUHENKAN")],
+    K("M-SPACE"): K("Super-SPACE"),
+}, "Global")
+
+
+# define_conditional_modmap(lambda wm_class: wm_class in ("FocusProxy"), {
+#     Key.CAPSLOCK: Key.LEFT_CTRL,
+#     Key.LEFT_CTRL: Key.CAPSLOCK,
+# 
+#     Key.LEFT_ALT: Key.LEFT_META,
+#     # Key.RIGHT_ALT: Key.RIGHT_META,
+#     Key.LEFT_META: Key.LEFT_ALT,
+# })
+# 
+# define_keymap(lambda wm_class: wm_class in ("FocusProxy"), {
+#     K("Super-q"): K("M-q"),
+# })
+
+
