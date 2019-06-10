e94b2aa9c8eadd4acf3863d8a8263eda399ed36c
ikeikeikeike / Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2019-06-11 04:49:15 +0900

    update xkeysnail

--- .xkeysnail/config.py
+++ .xkeysnail/config.py
@@ -77,6 +77,7 @@ define_keymap(lambda wm_class: wm_class in ("Gnome-terminal"), {
 
 
 define_keymap(lambda wm_class: wm_class in ("Slack"), {
+    K("M-w"): K("C-w"),
     K("M-a"): K("C-a"),
     K("M-s"): K("C-s"),
     K("M-x"): K("C-x"),
